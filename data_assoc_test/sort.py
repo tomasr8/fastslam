6568b4cce2a7e1e36749fc56d53fad8e35cce3d6
Tomas Roun <tomas.roun@seznam.cz>
2021-01-16 00:01:29 +0100

    delete some shit

--- data_assoc_test/sort.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import time
-
-array = np.random.uniform(0, 1, size=5000000)
-start = time.time()
-np.sort(array)
-print(time.time() - start)
--- data_assoc_test/update.py
+++
@@ -1,8 +0,0 @@
-import pycuda.driver as cuda
-from pycuda.compiler import SourceModule
-import pycuda.autoinit
-
-with open("update2.cu", "r") as f:
-    source = f.read()
-
-cuda_update = SourceModule(source)

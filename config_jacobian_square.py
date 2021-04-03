fix jacobian, simplify matching, add new map

config_jacobian_square.py
@@ -3,27 +3,37 @@ from utils import dotify
 from cuda.update_jacobian_dist import load_cuda_modules
 
 config = {
-    "SEED": 0,
-    "N": 512,  # number of particles
+    "SEED": 1,
+    "N": 4096,  # number of particles
     "DT": 0.5,
     "THREADS": 512,  # number threads in a block
     "GPU_HEAP_SIZE_BYTES": 100000 * 1024,  # available GPU heap size
-    "THRESHOLD": 0.7,
+    "THRESHOLD": 1.2,
     "sensor": {
         "RANGE": 5,
         "FOV": np.pi,
         "MISS_PROB": 0,  # probability landmark in range will be missed
-        "VARIANCE": [0.03, 0.002],
+        "VARIANCE": [0.05, 0.003],
         "MAX_MEASUREMENTS": 50  # upper bound on the total number of simultaneous measurements
     },
+    # "gps": {
+    #     "VARIANCE": [0.1, 0.1, 0.01],
+    #     "RATE": 50
+    # },
     "CONTROL": np.vstack((
         np.tile([0.0, 0.0], (15, 1)),
-        np.tile([0.12, 0.7], (100, 1))
+        np.tile([0.0, 0.5], (70, 1)),
+        np.tile([0.21, 0.0], (14, 1)),
+        np.tile([0.0, 0.5], (70, 1)),
+        np.tile([0.21, 0.0], (14, 1)),
+        np.tile([0.0, 0.5], (70, 1)),
+        np.tile([0.21, 0.0], (14, 1)),
+        np.tile([0.0, 0.5], (70, 1))
     )),
-    "CONTROL_VARIANCE": [0.01, 0.05],
-    "LANDMARKS": np.loadtxt("landmarks.txt").astype(np.float32),  # landmark positions
+    "CONTROL_VARIANCE": [0.02, 0.1],
+    "LANDMARKS": np.loadtxt("landmarks_square.txt").astype(np.float32),  # landmark positions
     "MAX_LANDMARKS": 250,  # upper bound on the total number of landmarks in the environment
-    "START_POSITION": np.array([8, 3, 0], dtype=np.float32)
+    "START_POSITION": np.array([1, 1, 0], dtype=np.float32)
 }
 
 config = dotify(config)

fastslam_jacobian.py
@@ -1,5 +1,6 @@
 import math
 import time
+import json
 import numpy as np
 import scipy
 import scipy.stats
@@ -17,21 +18,19 @@ import pycuda.autoinit
 from pycuda.autoinit import context
 from pycuda.driver import limit
 
-from cuda.update_jacobian import load_cuda_modules
+from cuda.update_jacobian_dist import load_cuda_modules
 from sensor import Sensor
 from vehicle import Vehicle
 from stats import Stats
 from common import CUDAMemory, resample, rescale, get_pose_estimate
-from config_jacobian import config
 from utils import repeat
 
-def run_SLAM(plot=False, seed=None):
+
+def run_SLAM(config, plot=False, seed=None):
     if seed is None:
         np.random.seed(config.SEED)
     else:
         np.random.seed(seed)
-
-    context.set_limit(limit.MALLOC_HEAP_SIZE, config.GPU_HEAP_SIZE_BYTES)
 
     assert config.THREADS <= 1024  # cannot run more in a single block
     assert config.N >= config.THREADS
@@ -52,15 +51,10 @@ def run_SLAM(plot=False, seed=None):
     )
 
     vehicle = Vehicle(config.START_POSITION, config.CONTROL_VARIANCE, dt=config.DT)
+    cuda_modules = config.modules
 
     memory = CUDAMemory(config)
     weights = np.zeros(config.N, dtype=np.float64)
-
-    cuda_modules = load_cuda_modules(
-        THREADS=config.THREADS,
-        PARTICLE_SIZE=config.PARTICLE_SIZE,
-        N_PARTICLES=config.N
-    )
 
     cuda.memcpy_htod(memory.cov, config.sensor.COVARIANCE)
     cuda.memcpy_htod(memory.particles, particles)
@@ -71,7 +65,7 @@ def run_SLAM(plot=False, seed=None):
 
 
     stats = Stats("Loop", "Measurement")
-    stats.add_pose(config.START_POSITION, config.START_POSITION)
+    stats.add_pose(config.START_POSITION.tolist(), config.START_POSITION.tolist())
     print("starting..")
 
 
@@ -91,13 +85,21 @@ def run_SLAM(plot=False, seed=None):
 
         cuda.memcpy_htod(memory.measurements, visible_measurements)
 
-        cuda_modules["predict"].get_function("predict_from_model")(
-            memory.particles,
-            np.float32(config.CONTROL[i, 0]), np.float32(config.CONTROL[i, 1]),
-            np.float32(config.CONTROL_VARIANCE[0]), np.float32(config.CONTROL_VARIANCE[1]),
-            np.float32(config.DT),
-            block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
-        )
+        if "gps" in config and i % config.gps.RATE == 0:
+            cuda_modules["predict"].get_function("predict_from_imu")(
+                memory.particles,
+                np.float32(vehicle.position[0]), np.float32(vehicle.position[1]), np.float32(vehicle.position[2]),
+                np.float32(config.gps.VARIANCE[0]), np.float32(config.gps.VARIANCE[1]), np.float32(config.gps.VARIANCE[2]),
+                block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
+            )
+        else:
+            cuda_modules["predict"].get_function("predict_from_model")(
+                memory.particles,
+                np.float32(config.CONTROL[i, 0]), np.float32(config.CONTROL[i, 1]),
+                np.float32(config.CONTROL_VARIANCE[0]), np.float32(config.CONTROL_VARIANCE[1]),
+                np.float32(config.DT),
+                block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
+            )
 
         cuda_modules["update"].get_function("update")(
             memory.particles, np.int32(config.N//config.THREADS),
@@ -113,7 +115,7 @@ def run_SLAM(plot=False, seed=None):
         rescale(cuda_modules, config, memory)
         estimate =  get_pose_estimate(cuda_modules, config, memory)
 
-        stats.add_pose(vehicle.position, estimate)
+        stats.add_pose(vehicle.position.tolist(), estimate.tolist())
 
         if plot:
             cuda.memcpy_dtoh(particles, memory.particles)
@@ -155,7 +157,14 @@ def run_SLAM(plot=False, seed=None):
             for i, landmark in enumerate(FlatParticle.get_landmarks(particles, best)):
                 plot_confidence_ellipse(ax[1], landmark, covariances[i], n_std=3)
 
-            plt.pause(0.01)
+            plt.pause(0.001)
+
+
+        if i == config.CONTROL.shape[0]-1:
+            cuda.memcpy_dtoh(particles, memory.particles)
+            best = np.argmax(FlatParticle.w(particles))
+            best_covariances = FlatParticle.get_covariances(particles, best)
+            best_landmarks = FlatParticle.get_landmarks(particles, best)
 
 
         cuda_modules["weights_and_mean"].get_function("get_weights")(
@@ -174,9 +183,37 @@ def run_SLAM(plot=False, seed=None):
 
         stats.stop_measuring("Loop")
 
+
+    if not plot:
+        fig, ax = plt.subplots()
+        plot_history(ax, stats.ground_truth_path, color='green')
+        plot_history(ax, stats.predicted_path, color='orange')
+        plot_landmarks(ax, config.LANDMARKS, color="blue")
+        plot_map(ax, best_landmarks, color="orange", marker="o")
+        for i, landmark in enumerate(best_landmarks):
+            plot_confidence_ellipse(ax, landmark, best_covariances[i], n_std=3)
+
+        plt.savefig(f"figs_jacobi_dist/{seed}.png")
+
+    # output = {
+    #     "ground": stats.ground_truth_path,
+    #     "predicted": stats.predicted_path,
+    #     "landmarks": config.LANDMARKS.tolist()
+    # }
+
+    # with open("out_jacobian.json", "w") as f:
+    #     json.dump(output, f)
+
     stats.summary()
     return stats.mean_path_deviation()
 
 
 if __name__ == "__main__":
-    run_SLAM(plot=True)
+    # from config_jacobian import config
+    from config_jacobian_square import config
+    context.set_limit(limit.MALLOC_HEAP_SIZE, config.GPU_HEAP_SIZE_BYTES)
+
+    run_SLAM(config, plot=True)
+
+    # context.set_limit(limit.MALLOC_HEAP_SIZE, config.GPU_HEAP_SIZE_BYTES)
+    # print(repeat(run_SLAM, seeds=np.arange(100)))

fastslam_model.py
@@ -1,5 +1,6 @@
 import math
 import time
+import json
 import numpy as np
 import scipy
 import scipy.stats
@@ -17,21 +18,17 @@ import pycuda.autoinit
 from pycuda.autoinit import context
 from pycuda.driver import limit
 
-from cuda.update3 import load_cuda_modules
 from sensor import Sensor
 from vehicle import Vehicle
 from stats import Stats
 from common import CUDAMemory, resample, rescale, get_pose_estimate
-from config_model import config
 from utils import repeat
 
-def run_SLAM(plot=False, seed=None):
-    if seed is None:
-        np.random.seed(config.SEED)
-    else:
-        np.random.seed(seed)
 
-    context.set_limit(limit.MALLOC_HEAP_SIZE, config.GPU_HEAP_SIZE_BYTES)
+def run_SLAM(config, plot=False, seed=None):
+    if seed is None:
+        seed = config.SEED
+    np.random.seed(seed)
 
     assert config.THREADS <= 1024 # cannot run more in a single block
     assert config.N >= config.THREADS
@@ -52,15 +49,10 @@ def run_SLAM(plot=False, seed=None):
     )
 
     vehicle = Vehicle(config.START_POSITION, config.CONTROL_VARIANCE, dt=config.DT)
+    cuda_modules = config.modules
 
     memory = CUDAMemory(config)
     weights = np.zeros(config.N, dtype=np.float64)
-
-    cuda_modules = load_cuda_modules(
-        THREADS=config.THREADS,
-        PARTICLE_SIZE=config.PARTICLE_SIZE,
-        N_PARTICLES=config.N
-    )
 
     cuda.memcpy_htod(memory.cov, config.sensor.COVARIANCE)
     cuda.memcpy_htod(memory.particles, particles)
@@ -71,7 +63,7 @@ def run_SLAM(plot=False, seed=None):
 
 
     stats = Stats("Loop", "Measurement")
-    stats.add_pose(config.START_POSITION, config.START_POSITION)
+    stats.add_pose(config.START_POSITION.tolist(), config.START_POSITION.tolist())
     print("starting..")
 
 
@@ -91,13 +83,21 @@ def run_SLAM(plot=False, seed=None):
 
         cuda.memcpy_htod(memory.measurements, visible_measurements)
 
-        cuda_modules["predict"].get_function("predict_from_model")(
-            memory.particles,
-            np.float32(config.CONTROL[i, 0]), np.float32(config.CONTROL[i, 1]),
-            np.float32(config.CONTROL_VARIANCE[0]), np.float32(config.CONTROL_VARIANCE[1]),
-            np.float32(config.DT),
-            block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
-        )
+        if "gps" in config and i % config.gps.RATE == 0:
+            cuda_modules["predict"].get_function("predict_from_imu")(
+                memory.particles,
+                np.float32(vehicle.position[0]), np.float32(vehicle.position[1]), np.float32(vehicle.position[2]),
+                np.float32(config.gps.VARIANCE[0]), np.float32(config.gps.VARIANCE[1]), np.float32(config.gps.VARIANCE[2]),
+                block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
+            )
+        else:
+            cuda_modules["predict"].get_function("predict_from_model")(
+                memory.particles,
+                np.float32(config.CONTROL[i, 0]), np.float32(config.CONTROL[i, 1]),
+                np.float32(config.CONTROL_VARIANCE[0]), np.float32(config.CONTROL_VARIANCE[1]),
+                np.float32(config.DT),
+                block=(config.THREADS, 1, 1), grid=(config.N//config.THREADS, 1, 1)
+            )
 
         cuda_modules["update"].get_function("update")(
             memory.particles, np.int32(config.N//config.THREADS),
@@ -113,15 +113,13 @@ def run_SLAM(plot=False, seed=None):
         rescale(cuda_modules, config, memory)
         estimate =  get_pose_estimate(cuda_modules, config, memory)
 
-        stats.add_pose(vehicle.position, estimate)
+        stats.add_pose(vehicle.position.tolist(), estimate.tolist())
 
         if plot:
             cuda.memcpy_dtoh(particles, memory.particles)
 
             ax[0].clear()
             ax[1].clear()
-            # ax[0].set_axis_off()
-            # ax[1].set_axis_off()
             ax[0].axis('scaled')
             ax[1].axis('scaled')
 
@@ -150,7 +148,14 @@ def run_SLAM(plot=False, seed=None):
             for i, landmark in enumerate(FlatParticle.get_landmarks(particles, best)):
                 plot_confidence_ellipse(ax[1], landmark, covariances[i], n_std=3)
 
-            plt.pause(0.01)
+            plt.pause(0.001)
+
+
+        if i == config.CONTROL.shape[0]-1:
+            cuda.memcpy_dtoh(particles, memory.particles)
+            best = np.argmax(FlatParticle.w(particles))
+            best_covariances = FlatParticle.get_covariances(particles, best)
+            best_landmarks = FlatParticle.get_landmarks(particles, best)
 
 
         cuda_modules["weights_and_mean"].get_function("get_weights")(
@@ -169,10 +174,61 @@ def run_SLAM(plot=False, seed=None):
 
         stats.stop_measuring("Loop")
 
+
+    if not plot:
+        fig, ax = plt.subplots()
+        plot_history(ax, stats.ground_truth_path, color='green')
+        plot_history(ax, stats.predicted_path, color='orange')
+        plot_landmarks(ax, config.LANDMARKS, color="blue")
+        plot_map(ax, best_landmarks, color="orange", marker="o")
+        for i, landmark in enumerate(best_landmarks):
+            plot_confidence_ellipse(ax, landmark, best_covariances[i], n_std=3)
+
+        # latest_weights = FlatParticle.w(particles)
+        # w = []
+        # data = []
+
+        # for i in range(config.N):
+        #     for landmark in FlatParticle.get_landmarks(particles, i):
+        #         data.append(landmark)
+        #         w.append(latest_weights[i])
+
+        # data = np.array(data)
+        # w = np.array(w)
+        # w = np.ones(len(data), dtype=np.float)
+
+        # from gmm import GMM
+
+        # gmm = GMM(
+        #     n_components = len(best_landmarks), n_iters = 1, tol = 1e-4, seed = 4,
+        #     init_means=best_landmarks, init_covs=best_covariances
+        # )
+        # gmm.fit(data)
+
+        # # means, covs = gmm(data[:2000], best_landmarks, best_covariances)
+        # plot_map(ax, gmm.means, color="purple", marker="o")
+        # for i, landmark in enumerate(gmm.means):
+        #     plot_confidence_ellipse(ax, landmark, gmm.covs[i], n_std=3)
+
+        plt.savefig(f"figs_model/{seed}.png")
+        print(f"figs_model/{seed}.png")
+
+    # output = {
+    #     "ground": stats.ground_truth_path,
+    #     "predicted": stats.predicted_path,
+    #     "landmarks": config.LANDMARKS.tolist()
+    # }
+
+    # with open("out_model.json", "w") as f:
+    #     json.dump(output, f)
+
     stats.summary()
     return stats.mean_path_deviation()
 
 
 if __name__ == "__main__":
-    # repeat(run_SLAM, times=100, seed=0)
-    run_SLAM(plot=True)
+    from config_model_square import config
+    # from config_model_square import config
+    context.set_limit(limit.MALLOC_HEAP_SIZE, config.GPU_HEAP_SIZE_BYTES)
+    # print(repeat(run_SLAM, seeds=np.arange(100)))
+    run_SLAM(config, plot=False)

utils.py
@@ -14,12 +14,10 @@ def dotify(dictionary):
     })
 
 
-def repeat(slam, times=100, seed=0):
-    np.random.seed(seed)
-
+def repeat(slam, seeds):
     deviations = []
-    for _ in range(times):
-        deviation = slam(plot=False, seed=np.random.randint(0, 1e6))
+    for seed in seeds:
+        deviation = slam(plot=False, seed=seed)
         deviations.append(deviation)
 
     return np.mean(deviations), np.std(deviations)